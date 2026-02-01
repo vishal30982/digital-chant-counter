5c3027cab8932bd5c12e687478e876098a31478d
Utkarsh Sharma <Vishal30982@gmail.com>
2026-02-01 21:48:13 +0530

    added sfx of bell and virbation on every click

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+const audio = new Audio('bell-sfx.mp3');
+audio.volume = 0.02;
 document.body.onclick = function() {
     document.querySelector('h1#count').innerText = parseInt(document.querySelector('h1#count').innerText) + 1;
 
+    audio.currentTime = 0;
+    audio.play();
+
+    if ('vibrate' in navigator) {
+      navigator.vibrate(1);
+    }
+
     // fullscreen
     if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen().catch(err => {
-            alert(`Error attempting to enable fullscreen: ${err.message}`);
-        });
+      document.documentElement.requestFullscreen().catch(err => {
+        alert(`Error attempting to enable fullscreen: ${err.message}`);
+      });
     }
 }
 
